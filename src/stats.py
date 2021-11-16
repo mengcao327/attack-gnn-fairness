b1630eba9cf2bf662fc0c3a47c610abe5afe846d
Hussain Hussain <hhussain@know-center.at>
2021-11-16 16:23:49 +0100

    fixed homophily + added community correlation

--- src/stats.py
+++ src/stats.py
@@ -3,6 +3,7 @@ from utils import *
 import networkx as nx
 from attack.attack import attack
 import pandas as pd
+from community.community_louvain import best_partition
 
 
 def homophily(G, prop, level='macro'):
@@ -14,18 +15,68 @@ def homophily(G, prop, level='macro'):
     :return:
     """
     if level == 'macro':
-        return len([edge for edge in G.edges() if prop[edge[0]] == prop[edge[1]]]) / G.number_of_edges()
+        return len([edge for edge in G.edges() if prop[edge[0]]!=-1 and prop[edge[0]] == prop[edge[1]]]) / len([edge for edge in G.edges() if prop[edge[0]]!=-1 and prop[edge[1]]!=-1])
     elif level == 'micro':
-        return sum([local_homophily(G, node, prop) for node in G.nodes()])
+        local_homophily_pairs = [local_homophily(G, node, prop) for node in G.nodes() if prop[node]!=-1]
+        nom = sum([x[0] for x in local_homophily_pairs])
+        den = sum([x[1] for x in local_homophily_pairs])
+        return nom/den
     else:
         print(f"Unrecognized level {level}, use \'macro\' or \'micro\'")
 
 
 def local_homophily(G, node, prop):
-    neighbors = list(G.neighbors(node))
+    neighbors = [u for u in G.neighbors(node) if prop[u]!=-1]
     d = len(neighbors)
-    if d == 0: return 0
-    return len([neighbor for neighbor in neighbors if prop[node] == prop[neighbor]]) / d / G.number_of_nodes()
+    if d == 0: return 0,0
+    return len([neighbor for neighbor in neighbors if prop[node]!=-1 and prop[node] == prop[neighbor]]) / d,1
+
+
+def calc_uncertainty(df_community):
+    
+    def agg(x):
+        return len(x.unique())
+
+    communities = df_community.community.unique()
+    labels = df_community.label.unique()
+
+    mtx = df_community.pivot_table(index='community', columns='label',values='node',aggfunc=agg).fillna(0) / len(df_community)
+    
+    def Pmarg(c):
+        return len(df_community[df_community.community == c]) / len(df_community)
+    
+    def Pcond(l,c):
+        return mtx.loc[c,l]/Pmarg(c)
+    
+    H = 0
+    for c in communities:
+        h = 0
+        for l in labels:
+            if Pcond(l,c) == 0:
+                continue
+            h += Pcond(l,c) * np.log2(1./Pcond(l,c))
+        H += h * Pmarg(c)
+    
+    def Pl(l):
+        return len(df_community[df_community.label == l]) / len(df_community)
+    
+    Hl = 0
+    for l in labels:
+        if Pl(l) == 0:
+            continue
+        Hl += Pl(l) * np.log2(1./Pl(l))
+    
+    IG = Hl-H
+    return IG/Hl
+
+
+def community_correlation(G, prop, seeds=[42,0,1,2,100]):
+    ret = []
+    for seed in seeds:
+        community_mapping = best_partition(G,random_state=seed)
+        df_community = pd.DataFrame([[u,community_mapping[u],prop[u].item()] for u in G.nodes() if prop[u]!=-1],columns=['node','community','label'])
+        ret.append(calc_uncertainty(df_community))
+    return ret, seeds
 
 
 parser = argparse.ArgumentParser()
@@ -47,9 +98,14 @@ parser.add_argument(
         'german',
         'bail',
         'dblp'])
-parser.add_argument('--ptb_rate', type=float, nargs='+', default=[0.05],
+parser.add_argument(
+    '--stats_type',
+    type=str,
+    default='homophily',
+    choices=['homophily', 'community'])
+parser.add_argument('--ptb_rate', type=float, nargs='+', default=[0.05,0.1,0.15,0.2,0.25,0.3],
                     help="Attack perturbation rate [0-1]")
-parser.add_argument('--seed', type=int, nargs='+', default=[42, 0, 1],
+parser.add_argument('--seed', type=int, nargs='+', default=[42, 0, 1, 2, 100],
                     help="Attack seed")
 parser.add_argument('--train_percent', type=float, default=0.5,
                     help='Percentage of labeled data as train set.')
@@ -59,8 +115,10 @@ args = parser.parse_known_args()[0]
 
 df_stats = pd.DataFrame()
 for ptb_rate in (args.ptb_rate if args.attack_type != 'none' else [0]):
-    for seed in args.seed:
+    for seed in (args.seed if args.attack_type != 'none' else [0]):
+        print("===== Computing stats =====")
         print(args.dataset)
+        print(f'seed={seed} ptb_rate={ptb_rate}')
         adj, features, labels, idx_train, idx_val, idx_test, sens, idx_sens_train, dataset, sens_attr = \
             load_dataset(args, seed)
 
@@ -69,18 +127,30 @@ for ptb_rate in (args.ptb_rate if args.attack_type != 'none' else [0]):
                          seed, dataset)
 
         G = nx.from_scipy_sparse_matrix(adj)
+        if args.stats_type=='homophily': 
+            label_homophily_macro = homophily(G, labels, 'macro')
+            print(label_homophily_macro)
+            label_homophily_micro = homophily(G, labels, 'micro')
+            print(label_homophily_micro)
+            sens_homophily_macro = homophily(G, sens, 'macro')
+            print(sens_homophily_macro)
+            sens_homophily_micro = homophily(G, sens, 'micro')
+            print(sens_homophily_micro)
+            row = {"ptb_rate": ptb_rate, "seed": seed,
+                       "label_homophily_macro": label_homophily_macro, "label_homophily_micro": label_homophily_micro,
+                       "sens_homophily_macro": sens_homophily_macro, "sens_homophily_micro": sens_homophily_micro}
+        elif args.stats_type == 'community':
+            community_label_correlations,seeds = community_correlation(G, labels) # assume the same seeds parameter for the next call
+            community_sens_correlations,seeds = community_correlation(G, sens)
+            row = [{"ptb_rate": ptb_rate, "seed": seed,
+                   "community_label_correlation":community_label_correlation,
+                   "community_sens_correlation":community_sens_correlation,
+                   "seed_comm":seed_comm,
+                   } for community_label_correlation,community_sens_correlation,seed_comm in zip(community_label_correlations,community_sens_correlations,seeds)]
 
-        label_homophily_macro = homophily(G, labels, 'macro')
-        label_homophily_micro = homophily(G, labels, 'micro')
-        sens_homophily_macro = homophily(G, sens, 'macro')
-        sens_homophily_micro = homophily(G, sens, 'micro')
-
-        row = {"ptb_rate": ptb_rate, "seed": seed,
-               "label_homophily_macro": label_homophily_macro, "label_homophily_micro": label_homophily_micro,
-               "sens_homophily_macro": sens_homophily_macro, "sens_homophily_micro": sens_homophily_micro}
         df_stats = df_stats.append(row, ignore_index=True)
 
-fname = '../results/stats-' + str(args.dataset) + \
+fname = f'../results/stats-{args.stats_type}-' + str(args.dataset) + \
         '-' + str(args.attack_type) + '.csv'
 
 df_stats.to_csv(fname, index=False)
